d2f7a1f0ec118baab9e46aee0340272bb1d94f76
Aliaksandr Paukalas <alexander.povkolas@gmail.com>
2018-11-01 18:47:27 +0300

    Read params

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <math.h>
+
+
+
+#define __PROFILE__
+
+#ifdef __PROFILE__
+
+#include <cstdio>
+#include <fstream>
+
+#endif
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+#ifdef __PROFILE__
+    ifstream in("input");
+    cin.rdbuf(in.rdbuf());
+#endif
+
+    int n = 0;
+    vector<pair<int, int>> edges(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> edges[i].first>> edges[i].second;
+    }
+
+
+#ifdef __PROFILE__
+
+#endif
+
+    cout << endl;
+
     return 0;
 }
